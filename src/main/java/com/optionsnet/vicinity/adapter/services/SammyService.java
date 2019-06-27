34e421bfd82851b180543acaed0f39613c948bb0
Dimitris Karadimas <dimitris@karadimas.org>
2019-06-27 19:03:41 +0300

    License information

--- src/main/java/com/optionsnet/vicinity/adapter/services/SammyService.java
+++ src/main/java/com/optionsnet/vicinity/adapter/services/SammyService.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright © 2019 OptionsNet.
+ * All rights reserved.
+ *
+ * This file is part of SaMMY@VICINITY adapter.
+ *
+ * SaMMY@VICINITY adapter is free software: you can redistribute it
+ * and/or modify it under the terms of GNU GPL.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT ANY WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT, IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * See README file for the full disclaimer information and LICENSE file for full license information in the project root.
+ */
+
 package com.optionsnet.vicinity.adapter.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
